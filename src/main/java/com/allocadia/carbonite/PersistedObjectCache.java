bb6fd093689f38a42b709c67c3fb3792a15cfa03
Jason Bedard <jason.bedard@allocadia.com>
2016-07-08 18:32:46 -0700

    Refactor getObjectById ugliness into a single helper

--- src/main/java/com/allocadia/carbonite/PersistedObjectCache.java
+++ src/main/java/com/allocadia/carbonite/PersistedObjectCache.java
@@ -8,15 +8,16 @@ import java.util.function.Function;
 public class PersistedObjectCache {
     private final Map<Class<?>, Map<?, ?>> objectInstances = new HashMap<>();
 
+    @SuppressWarnings("unchecked")
+    private <T, I> Map<I, T> getInstanceCache(Class<T> clazz,/*UNUSED: just for generics*/ I id) {
+        return (Map<I, T>) objectInstances.computeIfAbsent(clazz, (c) -> new HashMap<>());
+    }
+
     public <T, I> T getObjectById(Class<T> clazz, I id) {
-        @SuppressWarnings("unchecked")
-        final Map<I, T> objectCache = (Map<I, T>) objectInstances.computeIfAbsent(clazz, (c) -> new HashMap<>());
-        return objectCache.get(Objects.requireNonNull(id));
+        return getInstanceCache(clazz, id).get(Objects.requireNonNull(id));
     }
 
     public <T, I> T getObjectById(Class<T> clazz, I id, Function<I, T> fetcher) {
-        @SuppressWarnings("unchecked")
-        final Map<I, T> objectCache = (Map<I, T>) objectInstances.computeIfAbsent(clazz, (c) -> new HashMap<>());
-        return objectCache.computeIfAbsent(Objects.requireNonNull(id), fetcher.andThen(Objects::requireNonNull));
+        return getInstanceCache(clazz, id).computeIfAbsent(Objects.requireNonNull(id), fetcher.andThen(Objects::requireNonNull));
     }
 }
